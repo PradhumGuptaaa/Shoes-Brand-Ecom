3e7e0705c252ae3911a36e7f5ff00653d0b3d95e
PradhumGuptaaa <pradumgupta098@gmail.com>
2025-10-23 16:23:24 +0530

    Rename all files with capital first letter

--- Client/src/config/index.js
+++
@@ -1,5 +0,0 @@
-const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://your-backend-vercel-url.vercel.app/api' // यह URL आपको backend deploy के बाद मिलेगा
-  : 'http://localhost:5000/api';
-
-export { API_BASE_URL };
