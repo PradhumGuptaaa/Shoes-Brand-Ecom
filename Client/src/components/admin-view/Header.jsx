42ec42e1c77575cb64bb32018ca82383430e978c
PradhumGuptaaa <pradumgupta098@gmail.com>
2025-10-25 17:49:40 +0530

    uploading in vercel

--- Client/src/components/admin-view/Header.jsx
+++ Client/src/components/admin-view/Header.jsx
@@ -1,56 +1,7 @@
-// import { AlignJustify, LogOut } from "lucide-react";
-// import { Button } from "../ui/button";
-// import { useNavigate } from "react-router-dom";
-
-// function AdminHeader({ setOpen }) {
-//   const navigate = useNavigate();
-
-//   const handleLogout = async () => {
-//     try {
-//       const response = await fetch("http://localhost:5000/auth/logout", {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//       });
-
-//       if (response.ok) {
-//         // Remove token from localStorage
-//         localStorage.removeItem("token");
-
-//         // Navigate to login page
-//         navigate("/");
-//       } else {
-//         alert("Logout failed. Please try again.");
-//       }
-//     } catch (error) {
-//       console.error("Logout Error:", error);
-//       alert("Something went wrong!");
-//     }
-//   };
-
-//   return (
-//     <header className="flex items-center justify-between px-4 py-3 bg-background border-b">
-//       <Button onClick={() => setOpen(true)} className="lg:hidden sm:block">
-//         <AlignJustify />
-//         <span className="sr-only">Toggle Menu</span>
-//       </Button>
-//       <div className="flex flex-1 justify-end">
-//         <Button
-//           onClick={handleLogout}
-//           className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow"
-//         >
-//           <LogOut />
-//           Logout
-//         </Button>
-//       </div>
-//     </header>
-//   );
-// }
-
-// export default AdminHeader;
-
+import React from "react";
 
 import { AlignJustify, LogOut } from "lucide-react";
-import { Button } from "../ui/button";
+import { Button } from "../ui/Button";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
